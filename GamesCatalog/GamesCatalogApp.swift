92c146008ef694d4424b2680125413a612bfaa3d
ridhaaf <ridhaaf@gmail.com>
2022-10-02 18:10:55 +0700

    Initial Commit

---
+++ GamesCatalog/GamesCatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamesCatalogApp.swift
+//  GamesCatalog
+//
+//  Created by Ridha Ahmad Firdaus on 02/10/22.
+//
+
+import SwiftUI
+
+@main
+struct GamesCatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
